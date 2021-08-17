2891bdddd52b4042ebae3c18b26b4aceb1520536
Chaitanya182k <gattu182k@gmail.com>
2021-08-17 10:46:14 +0530

    Added department class

---
+++ department.java
@@ -0,0 +1,7 @@
+public class Department
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Hello from Department");
+		}
+}
--- employee.java
+++ employee.java
@@ -2,6 +2,7 @@ public class Employee
 {
 	public static void main(String[] args)
 		{
-			System.out.println("Hello");
+			System.out.println("Hello Git Repo");
+			System.out.println("Hello from Employee");
 		}
 }
